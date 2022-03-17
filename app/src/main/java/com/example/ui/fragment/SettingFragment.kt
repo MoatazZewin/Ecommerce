4287be31d7c6e0957b60bf101963eb0bda6bcb32
fadysameh197 <fady.samehx19@gmail.com>
2022-03-17 04:08:33 +0200

    settings

---
+++ app/src/main/java/com/example/ui/fragment/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.ui.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ecommerce.R
+import com.example.ecommerce.databinding.FragmentSettingBinding
+import com.example.ui.activity.MainActivity
+import com.example.ui.fragment.favorite.FavoriteFragment
+import com.example.ui.settings.AboutFragment
+
+lateinit var binding: FragmentSettingBinding
+
+
+class SettingFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
